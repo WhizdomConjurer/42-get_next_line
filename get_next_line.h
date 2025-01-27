02dfc651de6671e7500840043acfb9bbd9451dde
Rene <riebschlaeger1998@gmail.com>
2025-01-27 14:24:02 +0100

    first steps

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: whi <whi@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/27 14:03:40 by whi               #+#    #+#             */
+/*   Updated: 2025/01/27 14:14:56 by whi              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 2048
+# endif
+
+char	*get_next_line(int ft, ...);
+
+
+#endif
